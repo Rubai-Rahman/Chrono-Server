aac03bc8713f15b7d4ee2b2a8c12b4d823504d7b
Rubai-Rahman <rubairahman1@gmail.com>
2022-06-03 07:51:43 +0600

    server

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f8dsb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("cronoClick");
+    const productCollection = database.collection("products");
+
+    //Get Products
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    //POST API
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
